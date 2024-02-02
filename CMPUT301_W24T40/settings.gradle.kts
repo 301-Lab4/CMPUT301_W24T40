3249c07467c954d9bbb55724487707a43bf2174f
Syrus Muhammed <syrus.muham@gmail.com>
2024-02-01 18:43:37 -0700

    Added the stupid app

---
+++ CMPUT301_W24T40/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CMPUT301_W24T40"
+include(":app")
